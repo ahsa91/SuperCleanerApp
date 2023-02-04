c7148f6e0a26d42aa06526e63e8f6b9e7d27f778
ahsa91 <ahsa91@gmail.com>
2023-02-04 18:46:55 +0000

    Asking for Permissions to Store Profile Image

--- SuperCleanerApp-master/SuperCleaner/app/src/main/java/my/supercleanerapp/ui/activities/UserProfileActivity.kt
+++ SuperCleanerApp-master/SuperCleaner/app/src/main/java/my/supercleanerapp/ui/activities/UserProfileActivity.kt
@@ -1,7 +1,13 @@
 package my.supercleanerapp.ui.activities
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import my.supercleanerapp.R
 import my.supercleanerapp.databinding.ActivityUserProfileBinding
 import my.supercleanerapp.models.User
@@ -10,7 +16,7 @@ import my.supercleanerapp.utils.Constants
 @Suppress("DEPRECATION")
 
 
-class UserProfileActivity : AppCompatActivity() {
+class UserProfileActivity : BaseActivity(), View.OnClickListener {
 
     private lateinit var binding:ActivityUserProfileBinding
 
@@ -36,5 +42,70 @@ class UserProfileActivity : AppCompatActivity() {
         binding.etEmail.isEnabled = false
         binding.etEmail.setText(userDetails.email)
 
+        //profile photo onclick listner
+        binding.ivUserPhoto.setOnClickListener(this@UserProfileActivity)
+
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+
+                R.id.iv_user_photo -> {
+
+                    // Here we will check if the permission is already allowed or we need to request for it.
+                    // First of all we will check the READ_EXTERNAL_STORAGE permission and if it is not allowed we will request for the same.
+                    if (ContextCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                        )
+                        == PackageManager.PERMISSION_GRANTED
+                    ) {
+
+                        showErrorSnackBar("You already have the storage permission.", false)
+                    } else {
+
+                        /*Requests permissions to be granted to this application. These permissions
+                         must be requested in your manifest, they should not be granted to your app,
+                         and they should have protection level*/
+
+                        ActivityCompat.requestPermissions(
+                            this,
+                            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                            Constants.READ_STORAGE_PERMISSION_CODE
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * This function will identify the result of runtime permission after the user allows or deny permission based on the unique code.
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.READ_STORAGE_PERMISSION_CODE) {
+            //If permission is granted
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                showErrorSnackBar("The storage permission is granted.", false)
+            } else {
+                //Displaying another toast if permission is not granted
+                Toast.makeText(
+                    this,
+                    resources.getString(R.string.read_storage_permission_denied),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
     }
 }
--- SuperCleanerApp-master/SuperCleaner/app/src/main/java/my/supercleanerapp/utils/Constants.kt
+++ SuperCleanerApp-master/SuperCleaner/app/src/main/java/my/supercleanerapp/utils/Constants.kt
@@ -11,4 +11,5 @@ object Constants {
     const val MYSUPERCLEANERAPP_PREFERENCES: String = "MyShopPalPrefs"
     const val LOGGED_IN_USERNAME: String = "logged_in_username"
     const val EXTRA_USER_DETAILS: String = "extra_user_details"
+    const val READ_STORAGE_PERMISSION_CODE = 2
 }
