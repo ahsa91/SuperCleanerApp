254ab15b4a26adc91ad0c4ebbe0f80bc28f102b5
ahsa91 <ahsa91@gmail.com>
2023-02-04 00:10:07 +0000

    renamed project package to attempt to fix firestore error

---
+++ SuperCleanerApp-master/SuperCleaner/app/src/main/java/my/supercleanerapp/ui/activities/LoginActivity.kt
@@ -0,0 +1,86 @@
+package my.supercleanerapp.ui.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.view.WindowManager
+import my.supercleanerapp.R
+import my.supercleanerapp.databinding.ActivityLoginBinding
+
+@Suppress("DEPRECATION")
+class LoginActivity : BaseActivity(), View.OnClickListener {
+
+    /**
+     * This function is auto created by Android when the Activity Class is created.
+     */
+    private lateinit var binding:ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //This call the parent constructor
+        super.onCreate(savedInstanceState)
+        // This is used to align the xml view to this class
+        binding=ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // This is used to hide the status bar and make the login screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // Click event assigned to Forgot Password text.
+        binding.tvForgotPassword.setOnClickListener(this)
+        // Click event assigned to Login button.
+        binding.btnLogin.setOnClickListener(this)
+        // Click event assigned to Register text.
+        binding.tvRegister.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+
+                R.id.tv_forgot_password -> {
+
+                    val intent = Intent(this@LoginActivity, ForgotPasswordActivity::class.java)
+                    startActivity(intent)
+                }
+
+                R.id.btn_login -> {
+
+
+                    validateLoginDetails()
+
+                }
+
+                R.id.tv_register -> {
+                    // Launch the register screen when the user clicks on the text.
+                    val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
+    }
+
+    /**
+     * A function to validate the login entries of a user.
+     */
+    private fun validateLoginDetails(): Boolean {
+        return when {
+            TextUtils.isEmpty(binding.etEmail.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+                false
+            }
+            TextUtils.isEmpty(binding.etPassword.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_password), true)
+                false
+            }
+            else -> {
+                showErrorSnackBar("Your details are valid.", false)
+                true
+            }
+        }
+    }
+}
---
+++ SuperCleanerApp-master/SuperCleaner/app/src/main/java/my/supercleanerapp/ui/activities/SplashActivity.kt
@@ -0,0 +1,33 @@
+package my.supercleanerapp.ui.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import my.supercleanerapp.R
+
+@Suppress("DEPRECATION")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // This is used to hide the status bar and make the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // Adding the handler to after the a task after some delay.
+        // It is deprecated in the API level 30.
+        Handler().postDelayed(
+            {
+                // Launch the Main Activity
+                startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+                finish() // Call this when your activity is done and should be closed.
+            },
+            2500
+        )
+    }
+}
