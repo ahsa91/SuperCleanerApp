189f98e222cc94c26945d74228db429c67f56e30
ahsa91 <ahsa91@gmail.com>
2023-02-05 22:03:28 +0000

    settings activity created layout

--- SuperCleanerApp-master/SuperCleaner/app/src/main/java/my/supercleanerapp/ui/fragments/DashboardFragment.kt
+++ SuperCleanerApp-master/SuperCleaner/app/src/main/java/my/supercleanerapp/ui/fragments/DashboardFragment.kt
@@ -1,17 +1,22 @@
 package my.supercleanerapp.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import my.supercleanerapp.R
+import my.supercleanerapp.ui.activites.SettingsActivity
 
+@Suppress("DEPRECATION")
 class DashboardFragment : Fragment() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // If we want to use the option menu in fragment we need to add it.
+        setHasOptionsMenu(true)
+    }
 
-    /*private lateinit var dashboardViewModel: DashboardViewModel*/
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,17 +24,31 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        /*dashboardViewModel =
-            ViewModelProviders.of(this).get(DashboardViewModel::class.java)*/
-
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
         val textView: TextView = root.findViewById(R.id.text_dashboard)
         textView.text = "This is dashboard Fragment"
-
-        /*dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })*/
         return root
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.dashboard_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        when (id) {
+
+            R.id.action_settings -> {
+
+
+                startActivity(Intent(activity, SettingsActivity::class.java))
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
