be65cb0081042935b0fb70149196201ef8f3b3fd
ahsa91 <ahsa91@gmail.com>
2023-02-09 22:39:10 +0000

    service image is uploaded from device storage memory

--- SuperCleanerApp-master/SuperCleaner/app/src/main/java/my/supercleanerapp/ui/activites/AddServiceActivity.kt
+++ SuperCleanerApp-master/SuperCleaner/app/src/main/java/my/supercleanerapp/ui/activites/AddServiceActivity.kt
@@ -1,17 +1,43 @@
 package my.supercleanerapp.ui.activites
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import my.supercleanerapp.R
 import my.supercleanerapp.databinding.ActivityAddServiceBinding
+import my.supercleanerapp.utils.Constants
+import my.supercleanerapp.utils.GlideLoader
+import java.io.IOException
 
-class AddServiceActivity : AppCompatActivity() {
+class AddServiceActivity :  BaseActivity(), View.OnClickListener {
+
+    // A global variable for URI of a selected image from phone storage.
+    private var mSelectedImageFileUri: Uri? = null
+
+    // A global variable for uploaded service image URL.
+    private var mServiceImageURL: String = ""
+
 
     private lateinit var binding:ActivityAddServiceBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityAddServiceBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        // Assign the click event to iv_add_update_service image.
+        binding.ivAddUpdateService.setOnClickListener(this)
+
+        // Assign the click event to submit button.
+        binding.btnSubmit.setOnClickListener(this)
     }
 
 
@@ -31,5 +57,100 @@ class AddServiceActivity : AppCompatActivity() {
 
         binding.toolbarAddServiceActivity.setNavigationOnClickListener { onBackPressed() }
     }
+
+    override fun onClick(v: View?) {
+
+        if (v != null) {
+            when (v.id) {
+
+                // The permission code is similar to the user profile image selection.
+                R.id.iv_add_update_service -> {
+                    if (ContextCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                        )
+                        == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        Constants.showImageChooser(this@AddServiceActivity)
+                    } else {
+                        /*Requests permissions to be granted to this application. These permissions
+                         must be requested in your manifest, they should not be granted to your app,
+                         and they should have protection level*/
+                        ActivityCompat.requestPermissions(
+                            this,
+                            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                            Constants.READ_STORAGE_PERMISSION_CODE
+                        )
+                    }
+                }
+
+//                R.id.btn_submit -> {
+//                    if (validateServiceDetails()) {
+//
+//                        uploadServiceImage()
+//                    }
+//                }
+            }
+        }
+    }
+
+    /**
+     * This function will identify the result of runtime permission after the user allows or deny permission based on the unique code.
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.READ_STORAGE_PERMISSION_CODE) {
+            //If permission is granted
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Constants.showImageChooser(this@AddServiceActivity)
+            } else {
+                //Displaying another toast if permission is not granted
+                Toast.makeText(
+                    this,
+                    resources.getString(R.string.read_storage_permission_denied),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK
+            && requestCode == Constants.PICK_IMAGE_REQUEST_CODE
+            && data!!.data != null
+        ) {
+
+            // Replace the add icon with edit icon once the image is selected.
+            binding.ivAddUpdateService.setImageDrawable(
+                ContextCompat.getDrawable(
+                    this@AddServiceActivity,
+                    R.drawable.ic_vector_edit
+                )
+            )
+
+            // The uri of selection image from phone storage.
+            mSelectedImageFileUri = data.data!!
+
+            try {
+                // Load the service image in the ImageView.
+                GlideLoader(this@AddServiceActivity).loadServicePicture(
+                    mSelectedImageFileUri!!,
+                    binding.ivServiceImage
+                )
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
 
 }
--- SuperCleanerApp-master/SuperCleaner/app/src/main/java/my/supercleanerapp/utils/GlideLoader.kt
+++ SuperCleanerApp-master/SuperCleaner/app/src/main/java/my/supercleanerapp/utils/GlideLoader.kt
@@ -30,4 +30,19 @@ class GlideLoader(val context: Context) {
         }
     }
 
+    /**
+     * A function to load image from Uri or URL for the service image.
+     */
+    fun loadServicePicture(image: Any, imageView: ImageView) {
+        try {
+            // Load the user image in the ImageView.
+            Glide
+                .with(context)
+                .load(image) // Uri or URL of the image
+                .centerCrop() // Scale type of the image.
+                .into(imageView) // the view in which the image will be loaded.
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
 }
