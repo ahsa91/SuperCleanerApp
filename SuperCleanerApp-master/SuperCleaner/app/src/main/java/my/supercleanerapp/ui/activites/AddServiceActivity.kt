3566b1a4c708fe93e83824d23b34f96be0eb2284
ahsa91 <ahsa91@gmail.com>
2023-02-09 22:11:28 +0000

    design add product activity

--- SuperCleanerApp-master/SuperCleaner/app/src/main/java/my/supercleanerapp/ui/activites/AddServiceActivity.kt
+++ SuperCleanerApp-master/SuperCleaner/app/src/main/java/my/supercleanerapp/ui/activites/AddServiceActivity.kt
@@ -3,10 +3,33 @@ package my.supercleanerapp.ui.activites
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import my.supercleanerapp.R
+import my.supercleanerapp.databinding.ActivityAddServiceBinding
 
 class AddServiceActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityAddServiceBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_service)
+        binding= ActivityAddServiceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+
+
+    /**
+     * A function for actionBar Setup.
+     */
+    private fun setupActionBar() {
+
+        setSupportActionBar(binding.toolbarAddServiceActivity)
+
+        val actionBar= supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
+        }
+
+        binding.toolbarAddServiceActivity.setNavigationOnClickListener { onBackPressed() }
+    }
+
 }
