bfa8ca74e71dc0c2b91e1f86f65ced52a4ea818e
ahsa91 <ahsa91@gmail.com>
2023-02-09 21:12:05 +0000

    preparing the add service activity and the menu that leads there

---
+++ SuperCleanerApp-master/SuperCleaner/app/src/main/java/my/supercleanerapp/ui/activites/AddServiceActivity.kt
@@ -0,0 +1,12 @@
+package my.supercleanerapp.ui.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import my.supercleanerapp.R
+
+class AddServiceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_service)
+    }
+}
--- SuperCleanerApp-master/SuperCleaner/app/src/main/java/my/supercleanerapp/ui/fragments/ServicesFragment.kt
+++ SuperCleanerApp-master/SuperCleaner/app/src/main/java/my/supercleanerapp/ui/fragments/ServicesFragment.kt
@@ -1,16 +1,24 @@
 package my.supercleanerapp.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import my.supercleanerapp.R
+import my.supercleanerapp.ui.activites.AddServiceActivity
+
+
+@Suppress("DEPRECATION")
 
 class ServicesFragment : Fragment() {
 
     /*private lateinit var homeViewModel: HomeViewModel*/
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,14 +29,33 @@ class ServicesFragment : Fragment() {
             ViewModelProviders.of(this).get(HomeViewModel::class.java)*/
 
         val root = inflater.inflate(R.layout.fragment_services, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        textView.text = "This is home Fragment"
+        val textView: TextView = root.findViewById(R.id.text_service)
+        textView.text = "This is service Fragment"
 
         /*homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })*/
 
         return root
+    }
+
+    //verride the onCreateOptionsMenu function and inflate the Add Service menu.
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_service_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == R.id.action_add_service) {
+            // Launch the add service activity.
+
+            startActivity(Intent(activity, AddServiceActivity::class.java))
+
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
